0a3eb9d811a1ecace6b617e83d94185fcdf15fef
litongjava <litongjava@qq.com>
2025-03-05 22:23:22 -1000

    add LinkedInService

--- src/main/java/com/litongjava/llm/service/LinkedInService.java
+++ src/main/java/com/litongjava/llm/service/LinkedInService.java
@@ -2,6 +2,7 @@ package com.litongjava.llm.service;
 
 import org.postgresql.util.PGobject;
 
+import com.alibaba.fastjson2.JSONArray;
 import com.litongjava.apify.ApiFyClient;
 import com.litongjava.db.activerecord.Db;
 import com.litongjava.db.activerecord.Row;
@@ -13,6 +14,9 @@ import com.litongjava.tio.utils.hutool.StrUtil;
 import com.litongjava.tio.utils.json.FastJson2Utils;
 import com.litongjava.tio.utils.snowflake.SnowflakeIdUtils;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class LinkedInService {
   public String profileScraper(String url) {
     String cacheName = "linkedin_profile_scraper";
@@ -31,7 +35,20 @@ public class LinkedInService {
     ResponseVo responseVo = ApiFyClient.linkedinProfileScraper(url);
     if (responseVo.isOk()) {
       profile = responseVo.getBodyString();
-      profile = FastJson2Utils.parseArray(profile).toJSONString();
+      if (profile.startsWith("[")) {
+        try {
+          profile = FastJson2Utils.parseArray(profile).toJSONString();
+        } catch (Exception e) {
+          log.error("Failed to parse:{},{}", profile, e.getMessage(), e);
+        }
+      } else {
+        try {
+          profile = FastJson2Utils.parseObject(profile).toJSONString();
+        } catch (Exception e) {
+          log.error("Failed to parse:{},{}", profile, e.getMessage(), e);
+        }
+      }
+
       EhCacheKit.put(cacheName, url, profile);
       Row row = Row.by("id", SnowflakeIdUtils.id()).set("source", url).set("profile_data", PgObjectUtils.json(profile));
       Db.save(AgentTableNames.linkedin_profile_cache, row);
@@ -39,7 +56,7 @@ public class LinkedInService {
     }
     return null;
   }
-  
+
   public String profilePostsScraper(String url) {
     String cacheName = "linkedin_profile_posts_scraper";
     String profile = EhCacheKit.getString(cacheName, url);
@@ -57,7 +74,22 @@ public class LinkedInService {
     ResponseVo responseVo = ApiFyClient.linkedinProfilePostsScraper(url);
     if (responseVo.isOk()) {
       profile = responseVo.getBodyString();
-      profile = FastJson2Utils.parseArray(profile).toJSONString();
+      if (profile.startsWith("[")) {
+        try {
+          JSONArray parseArray = FastJson2Utils.parseArray(profile);
+          profile = parseArray.toJSONString();
+        } catch (Exception e) {
+          log.error("Failed to parse:{},{}", profile, e.getMessage(), e);
+
+        }
+
+      } else {
+        try {
+          profile = FastJson2Utils.parseObject(profile).toJSONString();
+        } catch (Exception e) {
+          log.error("Failed to parse:{},{}", profile, e.getMessage(), e);
+        }
+      }
       EhCacheKit.put(cacheName, url, profile);
       Row row = Row.by("id", SnowflakeIdUtils.id()).set("source", url).set("data", PgObjectUtils.json(profile));
       Db.save(AgentTableNames.linkedin_profile_posts_cache, row);
