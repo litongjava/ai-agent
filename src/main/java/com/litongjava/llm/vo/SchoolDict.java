8fe7bd49f0280ac4e315161f20a808a5c1480d8f
litongjava <litongjava@qq.com>
2025-03-10 21:35:54 -1000

    update SchoolDict

--- src/main/java/com/litongjava/llm/vo/SchoolDict.java
+++ src/main/java/com/litongjava/llm/vo/SchoolDict.java
@@ -12,5 +12,29 @@ public class SchoolDict {
   private String full_name;
   private String abbr_name;
   private String bot_name;
+  private String professor_names;
+  private String class_names;
   private String domain_name;
+
+  public SchoolDict(Long id, String fullName) {
+    this.id = id;
+    this.full_name = fullName;
+  }
+
+  public SchoolDict(Long id, String fullName, String abbrName, String professor_names, String botName) {
+    this.id = id;
+    this.full_name = fullName;
+    this.abbr_name = abbrName;
+    this.professor_names = professor_names;
+    this.bot_name = botName;
+  }
+
+  public SchoolDict(Long id, String fullName, String abbrName, String professor_names, String class_names, String botName) {
+    this.id = id;
+    this.full_name = fullName;
+    this.abbr_name = abbrName;
+    this.professor_names = professor_names;
+    this.class_names = class_names;
+    this.bot_name = botName;
+  }
 }
