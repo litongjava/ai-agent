98659b2f10e8f06fb8ac025f4899528b61608483
litongjava <litongjava@qq.com>
2025-03-19 00:16:46 -1000

    update WebPageService

--- src/main/java/com/litongjava/llm/service/WebPageService.java
+++ src/main/java/com/litongjava/llm/service/WebPageService.java
@@ -1,14 +1,21 @@
 package com.litongjava.llm.service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import com.google.common.util.concurrent.Striped;
 import com.litongjava.db.activerecord.Db;
 import com.litongjava.db.activerecord.Row;
 import com.litongjava.llm.consts.AgentTableNames;
+import com.litongjava.llm.utils.PdfUtils;
 import com.litongjava.model.http.response.ResponseVo;
+import com.litongjava.tio.utils.http.HttpDownloadUtils;
 import com.litongjava.tio.utils.snowflake.SnowflakeIdUtils;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 public class WebPageService {
 
   public static final String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36";
+  Striped<Lock> locks = Striped.lock(1024);
 
   /**
    * 根据 URL 获取页面内容，提取 body 部分并转换为 Markdown 格式.
@@ -25,33 +33,54 @@ public class WebPageService {
    * @return 包含 Markdown 格式内容的 ResponseVo 对象
    */
   public ResponseVo get(String url) {
-    if (!url.endsWith(".pdf")) {
-      String sql = "SELECT text FROM %s WHERE url = ?";
-      sql = String.format(sql, AgentTableNames.web_page_cache);
-      String result = Db.queryStr(sql, url);
+    String sql = "SELECT text FROM %s WHERE url = ?";
+    sql = String.format(sql, AgentTableNames.web_page_cache);
+    String result = Db.queryStr(sql, url);
+    if (result != null) {
+      return ResponseVo.ok(result);
+    }
+
+    Lock lock = locks.get(url);
+    lock.lock();
+
+    try {
+      result = Db.queryStr(sql, url);
       if (result != null) {
         return ResponseVo.ok(result);
       }
-      try {
-        Connection connect = Jsoup.connect(url);
-        connect.userAgent(userAgent);
-        Document document = connect.get();
-        Element bodyElement = document.body();
-        //String bodyHtml = bodyElement.html(); // 获取body标签内的HTML内容
-        result = bodyElement.text(); // 获取body标签内的纯文本内容
-        Row newRow = new Row();
-        newRow.set("id", SnowflakeIdUtils.id()).set("url", url).set("text", result);
-        Db.save(AgentTableNames.web_page_cache, newRow);
-        // 转换为 Markdown 格式
-        //String markdown = MarkdownUtils.safeToMd(bodyHtml);
-        return ResponseVo.ok(result);
-      } catch (Exception e) {
-        log.error(e.getMessage(), e);
-        return null;
+
+      if (!url.endsWith(".pdf")) {
+        try {
+          Connection connect = Jsoup.connect(url);
+          connect.userAgent(userAgent);
+          Document document = connect.get();
+          Element bodyElement = document.body();
+          //String bodyHtml = bodyElement.html(); // 获取body标签内的HTML内容
+          result = bodyElement.text(); // 获取body标签内的纯文本内容
+
+        } catch (Exception e) {
+          log.error(e.getMessage(), e);
+          return null;
+        }
+      } else {
+        ByteArrayOutputStream stream = HttpDownloadUtils.download(url, null);
+        try {
+          result = PdfUtils.parseContent(stream.toByteArray());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
       }
-    } else {
-      return null;
+
+      Row newRow = new Row();
+      newRow.set("id", SnowflakeIdUtils.id()).set("url", url).set("text", result);
+      Db.save(AgentTableNames.web_page_cache, newRow);
+      // 转换为 Markdown 格式
+      //String markdown = MarkdownUtils.safeToMd(bodyHtml);
+      return ResponseVo.ok(result);
+    } finally {
+      lock.unlock();
     }
+
   }
 
   /**
